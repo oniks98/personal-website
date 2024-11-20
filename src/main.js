c13a438c1c4b24332362eefdd79a01bc864d0a18
BulatovaTati <kohodatat@gmail.com>
2024-11-20 11:33:28 +0200

    add hero js and gsap library

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './js/menu';
 import './js/modal';
 import './js/swiper';
 import './js/to-top';
+import './js/hero';
 import './js/collaboration';
 import './js/projects';
 import './js/questions';
