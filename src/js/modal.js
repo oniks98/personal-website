eaafece21485c0d3e19234f8594069bf5d033b03
misselzabet <lisa.kovalevskaya1999@gmail.com>
2024-11-21 18:05:41 +0100

    modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,48 +1,59 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   const modalButtons = document.querySelectorAll('.js-open-modal');
-//   const overlay = document.querySelector('.js-overlay-modal');
-//   const closeButtons = document.querySelectorAll('.js-modal-close');
 
-//   modalButtons.forEach(function (item) {
-//     item.addEventListener('click', function (e) {
-//       e.preventDefault();
+const modal = document.getElementById('modal');
+const closeModalButton = document.querySelector('.close-btn');
 
-//       const modalId = this.getAttribute('data-modal');
-//       const modalElem = document.querySelector(
-//         '.modal[data-modal="' + modalId + '"]'
-//       );
+function openModal() {
+  modal.style.display = 'flex';
 
-//       modalElem.classList.add('active');
-//       overlay.classList.add('active');
-//       document.body.classList.add('modal-open');
-//     });
-//   });
+  document.addEventListener('keydown', handleEscape);
+}
 
-//   closeButtons.forEach(function (item) {
-//     item.addEventListener('click', function () {
-//       const parentModal = this.closest('.modal');
 
-//       parentModal.classList.remove('active');
-//       overlay.classList.remove('active');
-//       document.body.classList.remove('modal-open');
-//     });
-//   });
 
-//   document.body.addEventListener(
-//     'keyup',
-//     function (e) {
-//       if (e.key === 'Escape') {
-//         document.querySelector('.modal.active').classList.remove('active');
-//         document.querySelector('.modal-overlay').classList.remove('active');
-//         document.body.classList.remove('modal-open');
-//       }
-//     },
-//     false
-//   );
+function closeModall() {
+  modal.style.display = 'none';
 
-//   overlay.addEventListener('click', function () {
-//     document.querySelector('.modal.active').classList.remove('active');
-//     this.classList.remove('active');
-//     document.body.classList.remove('modal-open');
-//   });
-// });
+  document.removeEventListener('keydown', handleEscape);
+}
+
+function handleEscape(event) {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+}
+
+closeModalButton.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+  iziToast.show({
+    theme: 'dark',
+    class: 'toastStyle',
+    title: 'Thank you for your interest in cooperation!',
+    message:
+      'The manager will contact you shortly to discuss further details and opportunities for cooperation. Please stay in touch.',
+    position: 'center',
+    timeout: 0,
+    close: true,
+    backgroundColor: 'rgba(28,29,32,1)',
+    overlay: true,
+    overlayColor: 'rgba(0, 0, 0, 0.6)',
+    progressBar: false,
+    closeOnEscape: true,
+    closeOnClick: false,
+    overlayClose: true,
+  });
+  iziToast.show({
+    class: 'izi',
+    title: 'Error',
+    message: `Error: ${error.message}`,
+    position: 'center',
+    progressBar: false,
+    closeOnEscape: true,
+    closeOnClick: true,
+    timeout: 0,
+  });
