5077df76ea9a86526abcfcf5dc1ef74d81130016
Yurii <oniks9821@gmail.com>
2024-11-22 08:07:10 +0200

    projects-mob2

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,127 +1,178 @@
+import icon from '../icons/icons.svg';
+
+import projectDesk03 from '../images/projects/projects-desk-03-min.png';
+import projectDesk03_2x from '../images/projects/projects-desk-03@2x-min.png';
+import projectMob03 from '../images/projects/projects-mob-03-min.png';
+import projectMob03_2x from '../images/projects/projects-mob-03@2x-min.png';
+import projectTabl03 from '../images/projects/projects-tabl-03-min.png';
+import projectTabl03_2x from '../images/projects/projects-tabl-03@2x-min.png';
+
+import projectDesk04 from '../images/projects/projects-desk-04-min.png';
+import projectDesk04_2x from '../images/projects/projects-desk-04@2x-min.png';
+import projectMob04 from '../images/projects/projects-mob-04-min.png';
+import projectMob04_2x from '../images/projects/projects-mob-04@2x-min.png';
+import projectTabl04 from '../images/projects/projects-tabl-04-min.png';
+import projectTabl04_2x from '../images/projects/projects-tabl-04@2x-min.png';
+
+import projectDesk05 from '../images/projects/projects-desk-05-min.png';
+import projectDesk05_2x from '../images/projects/projects-desk-05@2x-min.png';
+import projectMob05 from '../images/projects/projects-mob-05-min.png';
+import projectMob05_2x from '../images/projects/projects-mob-05@2x-min.png';
+import projectTabl05 from '../images/projects/projects-tabl-05-min.png';
+import projectTabl05_2x from '../images/projects/projects-tabl-05@2x-min.png';
+
+import projectDesk06 from '../images/projects/projects-desk-06-min.png';
+import projectDesk06_2x from '../images/projects/projects-desk-06@2x-min.png';
+import projectMob06 from '../images/projects/projects-mob-06-min.png';
+import projectMob06_2x from '../images/projects/projects-mob-06@2x-min.png';
+import projectTabl06 from '../images/projects/projects-tabl-06-min.png';
+import projectTabl06_2x from '../images/projects/projects-tabl-06@2x-min.png';
+
+import projectDesk07 from '../images/projects/projects-desk-07-min.png';
+import projectDesk07_2x from '../images/projects/projects-desk-07@2x-min.png';
+import projectMob07 from '../images/projects/projects-mob-07-min.png';
+import projectMob07_2x from '../images/projects/projects-mob-07@2x-min.png';
+import projectTabl07 from '../images/projects/projects-tabl-07-min.png';
+import projectTabl07_2x from '../images/projects/projects-tabl-07@2x-min.png';
+
+import projectDesk08 from '../images/projects/projects-desk-08-min.png';
+import projectDesk08_2x from '../images/projects/projects-desk-08@2x-min.png';
+import projectMob08 from '../images/projects/projects-mob-08-min.png';
+import projectMob08_2x from '../images/projects/projects-mob-08@2x-min.png';
+import projectTabl08 from '../images/projects/projects-tabl-08-min.png';
+import projectTabl08_2x from '../images/projects/projects-tabl-08@2x-min.png';
+
+import projectDesk09 from '../images/projects/projects-desk-09-min.png';
+import projectDesk09_2x from '../images/projects/projects-desk-09@2x-min.png';
+import projectMob09 from '../images/projects/projects-mob-09-min.png';
+import projectMob09_2x from '../images/projects/projects-mob-09@2x-min.png';
+import projectTabl09 from '../images/projects/projects-tabl-09-min.png';
+import projectTabl09_2x from '../images/projects/projects-tabl-09@2x-min.png';
+
 const images = [
   {
     alt: 'Mimino website',
-    img: './images/projects/projects-desk-03-min.png',
+    img: projectDesk03,
     sources: {
       mobile: {
-        '1x': './images/projects/projects-mob-03-min.png',
-        '2x': './images/projects/projects-mob-03@2x-min.png',
+        '1x': projectMob03,
+        '2x': projectMob03_2x,
       },
       tablet: {
-        '1x': './images/projects/projects-tabl-03-min.png',
-        '2x': './images/projects/projects-tabl-03@2x-min.png',
+        '1x': projectTabl03,
+        '2x': projectTabl03_2x,
       },
       desktop: {
-        '1x': './images/projects/projects-desk-03-min.png',
-        '2x': './images/projects/projects-desk-03@2x-min.png',
+        '1x': projectDesk03,
+        '2x': projectDesk03_2x,
       },
     },
   },
   {
     alt: 'Vyshyvanka vibes Landing Page',
-    img: './images/projects/projects-desk-04-min.png',
+    img: projectDesk04,
     sources: {
       mobile: {
-        '1x': './images/projects/projects-mob-04-min.png',
-        '2x': './images/projects/projects-mob-04@2x-min.png',
+        '1x': projectMob04,
+        '2x': projectMob04_2x,
       },
       tablet: {
-        '1x': './images/projects/projects-tabl-04-min.png',
-        '2x': './images/projects/projects-tabl-04@2x-min.png',
+        '1x': projectTabl04,
+        '2x': projectTabl04_2x,
       },
       desktop: {
-        '1x': './images/projects/projects-desk-04-min.png',
-        '2x': './images/projects/projects-desk-04@2x-min.png',
+        '1x': projectDesk04,
+        '2x': projectDesk04_2x,
       },
     },
   },
   {
     alt: 'Power pulse webservice',
-    img: './images/projects/projects-desk-05-min.png',
+    img: projectDesk05,
     sources: {
       mobile: {
-        '1x': './images/projects/projects-mob-05-min.png',
-        '2x': './images/projects/projects-mob-05@2x-min.png',
+        '1x': projectMob05,
+        '2x': projectMob05_2x,
       },
       tablet: {
-        '1x': './images/projects/projects-tabl-05-min.png',
-        '2x': './images/projects/projects-tabl-05@2x-min.png',
+        '1x': projectTabl05,
+        '2x': projectTabl05_2x,
       },
       desktop: {
-        '1x': './images/projects/projects-desk-05-min.png',
-        '2x': './images/projects/projects-desk-05@2x-min.png',
+        '1x': projectDesk05,
+        '2x': projectDesk05_2x,
       },
     },
   },
   {
     alt: 'Chego jewelry website',
-    img: './images/projects/projects-desk-06-min.png',
+    img: projectDesk06,
     sources: {
       mobile: {
-        '1x': './images/projects/projects-mob-06-min.png',
-        '2x': './images/projects/projects-mob-06@2x-min.png',
+        '1x': projectMob06,
+        '2x': projectMob06_2x,
       },
       tablet: {
-        '1x': './images/projects/projects-tabl-06-min.png',
-        '2x': './images/projects/projects-tabl-06@2x-min.png',
+        '1x': projectTabl06,
+        '2x': projectTabl06_2x,
       },
       desktop: {
-        '1x': './images/projects/projects-desk-06-min.png',
-        '2x': './images/projects/projects-desk-06@2x-min.png',
+        '1x': projectDesk06,
+        '2x': projectDesk06_2x,
       },
     },
   },
   {
     alt: 'Energy flow webservice ',
-    img: './images/projects/projects-desk-07-min.png',
+    img: projectDesk07,
     sources: {
       mobile: {
-        '1x': './images/projects/projects-mob-07-min.png',
-        '2x': './images/projects/projects-mob-07@2x-min.png',
+        '1x': projectMob07,
+        '2x': projectMob07_2x,
       },
       tablet: {
-        '1x': './images/projects/projects-tabl-07-min.png',
-        '2x': './images/projects/projects-tabl-07@2x-min.png',
+        '1x': projectTabl07,
+        '2x': projectTabl07_2x,
       },
       desktop: {
-        '1x': './images/projects/projects-desk-07-min.png',
-        '2x': './images/projects/projects-desk-07@2x-min.png',
+        '1x': projectDesk07,
+        '2x': projectDesk07_2x,
       },
     },
   },
   {
     alt: 'Starlight studio landing page',
-    img: './images/projects/projects-desk-08-min.png',
+    img: projectDesk08,
     sources: {
       mobile: {
-        '1x': './images/projects/projects-mob-08-min.png',
-        '2x': './images/projects/projects-mob-08@2x-min.png',
+        '1x': projectMob08,
+        '2x': projectMob08_2x,
       },
       tablet: {
-        '1x': './images/projects/projects-tabl-08-min.png',
-        '2x': './images/projects/projects-tabl-08@2x-min.png',
+        '1x': projectTabl08,
+        '2x': projectTabl08_2x,
       },
       desktop: {
-        '1x': './images/projects/projects-desk-08-min.png',
-        '2x': './images/projects/projects-desk-08@2x-min.png',
+        '1x': projectDesk08,
+        '2x': projectDesk08_2x,
       },
     },
   },
   {
     alt: 'Fruitbox online store',
-    img: './images/projects/projects-desk-09-min.png',
+    img: projectDesk09,
     sources: {
       mobile: {
-        '1x': './images/projects/projects-mob-09-min.png',
-        '2x': './images/projects/projects-mob-09@2x-min.png',
+        '1x': projectMob09,
+        '2x': projectMob09_2x,
       },
       tablet: {
-        '1x': './images/projects/projects-tabl-09-min.png',
-        '2x': './images/projects/projects-tabl-09@2x-min.png',
+        '1x': projectTabl09,
+        '2x': projectTabl09_2x,
       },
       desktop: {
-        '1x': './images/projects/projects-desk-09-min.png',
-        '2x': './images/projects/projects-desk-09@2x-min.png',
+        '1x': projectDesk09,
+        '2x': projectDesk09_2x,
       },
     },
   },
@@ -188,7 +239,7 @@ function createMarkupProjects(images) {
             >
               <span class="projects-button_text">VISIT</span>
               <svg class="projects-button_icon" width="24" height="24">
-                <use href="./icons/icons.svg#icon-arrow-projects"></use>
+                <use href="${icon}#icon-arrow-projects"></use>
               </svg>
             </a>
           </div>
