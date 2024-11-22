4382200f7dff027ef4f53a4c5a33d72e5ecb7cd4
Yurii <oniks9821@gmail.com>
2024-11-22 21:16:33 +0200

    projects-desk2

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,3 +1,6 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 import icon from '../icons/icons.svg';
 
 import projectDesk03 from '../images/projects/projects-desk-03-min.png';
@@ -180,29 +183,42 @@ const images = [
 
 const listProjects = document.querySelector('.projects-list_img');
 const loadMoreButton = document.querySelector('.projects-button_loading');
-
-loadMoreButton.addEventListener('click', loadProjects);
+const boxLoader = document.querySelector('.box-loader');
 
 let loadedProjectsCount = 0;
 let previousCount = 3;
 
-function loadProjects() {
-  const batchSize = 3;
-  const remainingProjects = images.slice(
-    loadedProjectsCount,
-    loadedProjectsCount + batchSize
-  );
+loadMoreButton.addEventListener('click', loadProjects);
 
-  listProjects.insertAdjacentHTML(
-    'beforeend',
-    createMarkupProjects(remainingProjects)
-  );
+async function loadProjects() {
+  try {
+    toggleButton(loadMoreButton, false);
+    toggleSpinner(true);
 
-  loadedProjectsCount += remainingProjects.length;
-  scrollOnLoad();
+    const batchSize = 3;
+    const remainingProjects = images.slice(
+      loadedProjectsCount,
+      loadedProjectsCount + batchSize
+    );
 
-  if (loadedProjectsCount >= images.length) {
-    loadMoreButton.classList.add('hidden');
+    await listProjects.insertAdjacentHTML(
+      'beforeend',
+      createMarkupProjects(remainingProjects)
+    );
+
+    loadedProjectsCount += remainingProjects.length;
+
+    scrollOnLoad();
+
+    toggleButton(loadMoreButton, true);
+
+    if (loadedProjectsCount >= images.length) {
+      toggleButton(loadMoreButton, false);
+    }
+  } catch (error) {
+    showMessage('❌ Sorry, there was a server error. Please try again later!');
+  } finally {
+    toggleSpinner(false);
   }
 }
 
@@ -262,4 +278,21 @@ function scrollOnLoad() {
     });
   }
   previousCount += 3;
+}
+
+function toggleButton(button, isVisible) {
+  button.style.display = isVisible ? 'block' : 'none';
+}
+
+function toggleSpinner(isVisible) {
+  boxLoader.style.display = isVisible ? 'block' : 'none';
+}
+
+function showMessage(message) {
+  iziToast.show({
+    message,
+    color: 'red',
+    position: 'topRight',
+    timeout: 2000,
+  });
 }
